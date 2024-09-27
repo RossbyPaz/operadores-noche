bd2c049d948b60dfb7b9776f6068333d1327e7f5
ROSBY ALICIA PAZ JIMENEZ <1061806891@fup.edu>
2024-09-26 20:02:54 -0500

    Se agrega el archivo operadoresAsignacion al operadoresProjetc

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,27 @@
+#Ejemplos con Operadores de asignación
+
+var1= 5
+var2= 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1 += 3 #var1  = var1 +3 asignacion con suma
+print(var1)
+
+var2 -= 3 #var1  = var2 -3 asignacion con resta
+print(var2)
+
+var3 *= 3 #var3 = var3*3 asignacion con multiplicacion
+print(var3)
+
+var4 /= 3 #var1  = var1 /3 asignacion division
+print(var4)
+
+var5 %= 3 #var1  = var1 %3 asignacion con residuo
+print(var5)
+
+var6 //= 3 #var1  = var1 //3 asignacion con division entera
+print(var6)
+
