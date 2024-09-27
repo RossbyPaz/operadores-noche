c3da54debf08afc4c1ef4b24d62d424bac014bd3
ROSBY ALICIA PAZ JIMENEZ <1061806891@fup.edu>
2024-09-26 20:54:45 -0500

    Se agrega archivo OperadoresDeComparacion

---
+++ operadoresProject/OperadoreDeComparacion.py
@@ -0,0 +1,15 @@
+#Ejemplo con operadores de comparacion
+
+#m=45
+#n=23
+m=int(input("Ingrese el valor de m: "))
+n=int(input("Ingrese el valor de n: "))
+
+print(m==n) #Igualdad
+print(m > n) #Mayor que>
+print(m < n) #Menor que
+print(m >= n)#Mayor igual que >=
+print(m <= n) #Menor igual que<=
+print(m != n)#No igual !=
+
+
