eeb74f81233b6d53bac8f34467e3ac17eae3d456
ROSBY ALICIA PAZ JIMENEZ <1061806891@fup.edu>
2024-09-26 19:48:28 -0500

    Se crea el projecto operadoresProject y se guardan cambios

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,30 @@
+#Ejemplo operadores Aritmeticos
+#numero1:int = 18
+#numero2:int = 12
+numero1 = int(input("Ingrese el numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+suma=numero1 + numero2
+#print  (f"La suma es: ",suma )
+print(f"la suma de {numero1} y {numero2} es {suma}")
+
+
+num1 = int(input("Ingrese el numero1: "))
+num2 = int(input("Ingrese el numero2: "))
+resta=numero1 - numero2
+
+print(f"la resta de {num1} y {num2} es {resta}")
+
+
+n1 = int(input("Ingrese el numero1: "))
+n2 = int(input("Ingrese el numero2: "))
+division=n1 / n2
+
+print(f"la resta de {n1} y {n2} es {division}")
+
+
+
+nu1 = int(input("Ingrese el numero1: "))
+nu2 = int(input("Ingrese el numero2: "))
+multiplicacion=nu1 * nu2
+
+print(f"la multiplicacion de {nu1} y {nu2} es {multiplicacion}")
